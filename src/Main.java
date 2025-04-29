554c3ab32bc25c2922912f4b56bc49a10533a587
Добровольская Маша <dobrbobr89@gmail.com>
2025-04-29 15:02:15 +0400

    Task 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-
+        //task 1
+        System.out.println("Task 1");
+        //Имя
+        String firstName = "Ivan";
+        //Отчество
+        String middleName = "Ivanovich";
+        //Фамилия
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("______________");
+        //task 2
+        System.out.println("Task 2");
+        System.out.println(fullName.toUpperCase());
+        System.out.println("______________");
+        //task 3
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("______________");
     }
 }
